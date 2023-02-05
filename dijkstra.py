f043ee939f2f6835e539da40c7ce224eecf39106
Taj <sharma.tajinder01@gmail.com>
2023-02-04 19:12:51 -0800

    Decrease the number of nodes from 15 to 5:

--- dijkstra.py
+++ dijkstra.py
@@ -1,12 +1,13 @@
 import random
 
-num_nodes = 15
+num_nodes = 5
 graph = [[0 for _ in range(num_nodes)] for _ in range(num_nodes)]
 
 for i in range(num_nodes):
     for j in range(i, num_nodes):
         if i != j:
-            weight = random.randint(1, 100)
+            weight = random.randint(1, 25)
             graph[i][j] = weight
             graph[j][i] = weight
 
+print(graph)
