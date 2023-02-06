869b6a2656e1aadd5843bb89456a979a4fe12a05
Taj <sharma.tajinder01@gmail.com>
2023-02-05 16:47:03 -0800

    Intitialize starting g score values and f score values to inf

--- mazeProblem.py
+++ mazeProblem.py
@@ -1,4 +1,17 @@
 from pyamaze import maze 
+from queue import PriorityQueue
+
+def h(cell1,cell2):#calculates the heuristic value using manhattan distance equation
+    x1,y1 = cell1
+    x2,y2 = cell2 
+
+def aStar(maze):
+    start = (maze.rows,maze.cols)
+    g_score = {cell:float('inf') for cell in maze.grid}#sets g value for all nodes to inf at first bc we havent visited them yet
+    g_score[start] = 0 #gscore of starting val will be 0
+    f_score = {cell:float('inf') for cell in maze.grid}
+    f_score[start] = h(start,(1,1)) #f score from the start will be 0 + h(start to end) value 
+
 worldMap = maze(6,6)
 worldMap.CreateMaze()
 
